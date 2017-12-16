fe6ea241ef7df972fe28b54e1e9635d0ccf9c126
Tomasz Wojcik <Superstar@MacBook-Pro-Tomasz.local>
2017-12-16 11:21:36 +0100

    API call working with react-redux, able to console log data in the component

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,15 +2,26 @@
  * Created by Superstar on 16.12.2017.
  */
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchVideos } from '../ducks/videoList'
 
 class VideoList extends Component {
+    componentDidMount() {
+        this.props.fetchVideos()
+    }
+
     render () {
         return (
             <div>
+                {console.log(this.props.videos)}
                 Video List
             </div>
         )
     }
 }
 
-export default VideoList
+function mapStateToProps({ videos }) {
+    return { videos }
+}
+
+export default connect(mapStateToProps, { fetchVideos })(VideoList)
