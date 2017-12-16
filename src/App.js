e196b8ec647e736a67feea1cf40e2496ff2fb7ae
Tomasz Wojcik <Superstar@MacBook-Pro-Tomasz.local>
2017-12-16 10:49:27 +0100

    Initial commit - project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+    render() {
+        return (
+            <div>React setup</div>
+        )
+    }
+}
+
+
+export default App
---
+++ src/ducks/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Superstar on 16.12.2017.
+ */
+import { combineReducers } from 'redux'
+
+const rootReducer = combineReducers({
+    state: (state = {}) => state
+})
+
+export default rootReducer
